b89adfae83bf1f746de4b2c8723add61a35827e3
ac7644f282b8cb18d3cb784e4f4d6558203c12ee <krsjunk@netscape.net>
2021-03-04 07:59:10 -0600

    first buzz test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn fizzbuzz(i: u8) -> &'static str {
-	"fizz"
+	if i == 3 {
+		"fizz"
+	} else {
+		"buzz"
+	}
 }
 
 fn main() {
@@ -9,4 +13,9 @@ fn main() {
 #[test]
 fn fizzbuzz_returns_fizz_for_3() {
 	assert_eq!("fizz", fizzbuzz(3));
+}
+
+#[test]
+fn fizzbuzz_returns_buzz_for_5() {
+	assert_eq!("buzz", fizzbuzz(5));
 }
