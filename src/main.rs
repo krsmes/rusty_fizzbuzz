df8695cccaec91b9d5e4896d21331008bb888dfa
ac7644f282b8cb18d3cb784e4f4d6558203c12ee <krsjunk@netscape.net>
2021-03-04 09:52:09 -0600

    divisable_by tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-fn fizzbuzz(i: u8) -> &'static str {
-	if i == 3 {
-		"fizz"
+
+fn fizzbuzz(i: u8) -> String {
+	if i % 3 == 0 {
+		"fizz".to_string()
+	} else if i % 5 == 0  {
+		"buzz".to_string()
 	} else {
-		"buzz"
+		i.to_string()
 	}
 }
 
@@ -11,11 +14,24 @@ fn main() {
 }
 
 #[test]
-fn fizzbuzz_returns_fizz_for_3() {
-	assert_eq!("fizz", fizzbuzz(3));
+fn fizzbuzz_returns_fizz_for_divisible_by_3() {
+	for val in vec![3,6,9,12].iter() {
+		assert_eq!("fizz", fizzbuzz(*val));
+	}
 }
 
 #[test]
-fn fizzbuzz_returns_buzz_for_5() {
-	assert_eq!("buzz", fizzbuzz(5));
+fn fizzbuzz_returns_buzz_for_divisible_by_5() {
+	for val in vec![5,10].iter() {
+		assert_eq!("buzz", fizzbuzz(*val));
+	}
+}
+
+#[test]
+fn fizzbuzz_returns_1_for_1() {
+	assert_eq!("1", fizzbuzz(1));
+}
+#[test]
+fn fizzbuzz_returns_2_for_2() {
+	assert_eq!("2", fizzbuzz(2));
 }
